dbe15f604b9a2e4290cbd3391b488418ccc5b821
Rakhee99 <rakheemagic99@gmail.com>
2021-03-31 16:02:56 +0530

    added 106

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ insert into problem values (102,'Improvements or additions to documentation', 'd
 insert into problem values (103,'This issue or pull request already exists', 'duplicate');
 insert into problem values (104,'New feature or request', 'enhacement');
 insert into problem values (105,'This doesnt seem right', 'invalid');
+insert into problem values (106, 'need extra attention','help wanted');
